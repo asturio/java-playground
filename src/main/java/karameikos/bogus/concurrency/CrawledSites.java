83102886bc821a4af511aa80b9e0116993caaba8
Claudio Clemens <asturio@gmx.net>
2016-08-28 15:03:45 +0200

    First commit

---
+++ src/main/java/karameikos/bogus/concurrency/CrawledSites.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrawledSites {
+    private List<String> crawledSites = new ArrayList<>();
+    private List<String> linkedSites = new ArrayList<>();
+
+    public void add(String site) {
+        synchronized (this) {
+            if (!crawledSites.contains(site)) {
+                linkedSites.add(site);
+            }
+        }
+    }
+
+    public String next() {
+        if (linkedSites.isEmpty()) {
+            return null;
+        }
+
+        synchronized (this) {
+            if (!linkedSites.isEmpty()) {
+                String s = linkedSites.get(0);
+                linkedSites.remove(0);
+                crawledSites.add(s);
+                return s;
+            }
+            return null;
+        }
+    }
+}
---
+++ src/main/java/karameikos/bogus/concurrency/threads/MyRunnable.java
@@ -0,0 +1,7 @@
+public class MyRunnable implements Runnable {
+    private final long countUntil;
+
+    MyRunnable(long countUntil) {
+        this.countUntil = countUntil;
+    }
+}
