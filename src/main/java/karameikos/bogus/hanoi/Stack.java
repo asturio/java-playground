ff6327f6e0aaca0d0e7159dbf7aa5a75d72fd84c
Claudio Clemens <asturio@gmx.net>
2017-01-29 23:13:48 +0100

    - Changes version of JUnit - Added first test

---
+++ src/main/java/karameikos/bogus/hanoi/Stack.java
@@ -0,0 +1,12 @@
+package karameikos.bogus.hanoi;
+
+/**
+ * Created by claudio on 29.01.17.
+ */
+public class Stack {
+
+    @Override
+    public String toString() {
+        return "|-----";
+    }
+}
--- src/test/java/karameikos/bogus/hanoi/HanoiTest.java
+++ src/test/java/karameikos/bogus/hanoi/HanoiTest.java
@@ -1,4 +1,14 @@
 package karameikos.bogus.hanoi;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class HanoiTest {
+
+    @Test
+    public void testEmptyStack() {
+        Stack stack = new Stack();
+        Assert.assertEquals("Empty Stack", "|-----", stack.toString());
+    }
 }
